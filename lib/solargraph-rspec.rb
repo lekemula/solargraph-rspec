23e652d6836ec657e3e8359bc012858ada0300fb
Lekë Mula <l.mula@finlink.de>
2025-01-26 17:21:23 +0100

    Remove unused NodeParser

--- lib/solargraph-rspec.rb
+++ lib/solargraph-rspec.rb
@@ -6,13 +6,5 @@ require 'solargraph'
 require_relative 'solargraph/rspec/version'
 require_relative 'solargraph/rspec/convention'
 
-module Solargraph
-  module Rspec
-    class NodeParser
-      extend Solargraph::Parser::Legacy::ClassMethods
-    end
-  end
-end
-
 Solargraph::Convention.register Solargraph::Rspec::Convention
 # rubocop:enable Naming/FileName
