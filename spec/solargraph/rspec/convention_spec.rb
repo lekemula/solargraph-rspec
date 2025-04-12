1758de9da9b7cf2b66ac0b4f1d91c85deec8d038
Lekë Mula <l.mula@finlink.de>
2025-04-12 22:53:01 +0200

    solargraph 0.53.3 - Revert Adjust specs
    We seem to have the regression again - why?

--- spec/solargraph/rspec/convention_spec.rb
+++ spec/solargraph/rspec/convention_spec.rb
@@ -486,7 +486,9 @@ RSpec.describe Solargraph::Rspec::Convention do
     end
 
     it 'infers type for some_class' do
-      load_and_assert_type('let(:some_class) { Class.new }', 'some_class', 'Class<BasicObject>')
+      # FIXME: once https://github.com/castwide/solargraph/pull/786 gets released
+      # load_and_assert_type('let(:some_class) { Class.new }', 'some_class', 'Class<BasicObject>')
+      load_and_assert_type('let(:some_class) { Class.new }', 'some_class', 'Class')
     end
 
     it 'infers type for some_module' do
@@ -589,7 +591,9 @@ RSpec.describe Solargraph::Rspec::Convention do
       assert_public_instance_method_inferred_type(
         api_map,
         'RSpec::ExampleGroups::TestSomeNamespaceTransaction#some_class',
-        'Class<BasicObject>'
+        # FIXME: once https://github.com/castwide/solargraph/pull/786 gets released
+        # 'Class<BasicObject>'
+        'Class'
       )
     end
   end
