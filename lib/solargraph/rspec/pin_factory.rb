f8849a13a00635971e7156a31ed72ff81da79e62
Lekë Mula <l.mula@finlink.de>
2025-09-06 20:58:03 +0200

    Fix Credit typo

--- lib/solargraph/rspec/pin_factory.rb
+++ lib/solargraph/rspec/pin_factory.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Credits: This file was originally copied and adapted from the log_bench gem.
+# Credits: This file was originally copied and adapted from the solargraph-rails gem.
 
 module Solargraph
   module Rspec
