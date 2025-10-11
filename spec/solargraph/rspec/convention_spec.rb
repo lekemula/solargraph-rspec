2270c578410472b3063a3f116841bda48da01cc9
Vince Broz <vince@broz.cc>
2025-10-11 04:12:51 -0400

    Less limiting of gem pin processing in specs (#29)
    This matches a future Solargraph change which seems to be more
    conservative including gems while resolving constants - see
    https://github.com/castwide/solargraph/pull/1107 for an example
    failure.

--- spec/solargraph/rspec/convention_spec.rb
+++ spec/solargraph/rspec/convention_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Solargraph::Rspec::Convention do
   before do
     # For performance reasons, avoid solargraph loading all installed gems' YARDoc and RBS gem pins.
     # If your specs depend on YARDoc or RBS types, add them to the spec/solargraph/rspec/first_party_gem_helpers_spec.rb
-    avoid_gem_yard_and_rbs_pin_generation
+    allow(Solargraph::Rspec::Gems).to receive(:gem_names).and_return(%w[rspec])
   end
 
   it 'generates method for described_class' do
